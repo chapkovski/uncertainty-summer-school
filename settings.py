552506ba5bd722529377e66879b6fbe045fabab9
chapkovski <chapkovski@gmail.com>
2019-07-28 20:34:56 +0200

    update python intro

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 
-ROOMS = []
+ROOMS = [{'name'}]
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
