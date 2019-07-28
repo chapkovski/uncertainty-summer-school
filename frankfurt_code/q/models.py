f0cb332ba94abe2ccb8eed1a77b98abe9108e7bd
chapkovski <chapkovski@gmail.com>
2019-07-28 14:45:24 +0200

    final match pgg

--- frankfurt_code/q/models.py
+++ frankfurt_code/q/models.py
@@ -2,6 +2,7 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
+import random
 
 author = 'Your name here'
 
@@ -19,7 +20,12 @@ class Constants(BaseConstants):
 
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
+        for p in self.get_players():
+            choices = Constants.POLITY_CHOICES.copy()
+            random.shuffle(choices)
+            p.participant.vars['politics_choices'] = choices
+            p.politics_set = str(choices)
 
 
 class Group(BaseGroup):
@@ -30,5 +36,8 @@ class Player(BasePlayer):
     gender = models.IntegerField(label='Please indicate your gender', choices=Constants.GENDER_CHOICES,
                                  widget=widgets.RadioSelectHorizontal)
     politics = models.IntegerField(label='Who you are going to vote fore at 2020 elections?',
-                                   choices=Constants.POLITY_CHOICES,
                                    widget=widgets.RadioSelect)
+    politics_set = models.StringField(doc='To store the order political preferences are shown')
+
+    def politics_choices(self):
+        return self.participant.vars['politics_choices']
